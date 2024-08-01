7e9badf3464de3376faf805970e4dc25a25c0f01
Donistr <deniselag@mail.ru>
2024-08-01 22:17:24 +0400

    feat: сделал раскатку таблиц при запуске

---
+++ src/main/resources/db/changelog/data/roles.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO roles (id) VALUES
+	 ('USER'),
+	 ('CREDIT_USER'),
+	 ('OVERDRAFT_USER'),
+	 ('DEAL_SUPERUSER'),
+	 ('CONTRACTOR_RUS'),
+	 ('CONTRACTOR_SUPERUSER'),
+     ('SUPERUSER'),
+     ('ADMIN');
