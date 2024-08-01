af48853e92dda49d2d7e0977f83c2f0fc71bb3d7
Donistr <deniselag@mail.ru>
2024-08-02 03:36:04 +0400

    feat: добавил файлы для тестов

---
+++ src/main/resources/db/changelog/test/data/users.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO users (username,password,email) VALUES
+	 ('admin','$2a$10$GIwsGZRx/PaJqbhjDdbBSOqsQ5Anq.31.ayjmwg6NOKsjXbchjYpK','admin'),
+	 ('username','$2y$10$xBHgkRpRSBHo2eReB3eVV..BNEq9TqK.SbD0ByU4Is1ry3HxvgogO','username');
